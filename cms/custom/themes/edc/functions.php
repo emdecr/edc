ac1aabf72e42a38ad4887c87c363b64241fa2ac8
Emily Dela Cruz <delacruz.emily@gmail.com>
2019-10-05 19:17:37 -0400

    Update functions - cms

--- cms/custom/themes/edc/functions.php
+++ cms/custom/themes/edc/functions.php
@@ -4,6 +4,9 @@
 require_once('includes/functions/enqueue-style.php');
 require_once('includes/functions/enqueue-script.php');
 
+// Add metabox
+require_once('includes/functions/meta_box/mb_settings.php');
+
 // Add custom endpoints
 require_once('includes/functions/endpoints/pinterest.php');
 ?>
