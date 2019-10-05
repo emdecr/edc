8b606c3303c7ff6861ce15a301b752d50f8a0cb5
Emily Dela Cruz <delacruz.emily@gmail.com>
2019-10-05 19:18:43 -0400

    Update fxns

--- cms/custom/themes/edc/functions.php
+++ cms/custom/themes/edc/functions.php
@@ -5,7 +5,7 @@ require_once('includes/functions/enqueue-style.php');
 require_once('includes/functions/enqueue-script.php');
 
 // Add metabox
-require_once('includes/functions/metabox/mb_settings.php');
+require_once('includes/functions/metabox/mb-settings.php');
 
 // Add custom endpoints
 require_once('includes/functions/endpoints/pinterest.php');
