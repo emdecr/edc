b53ead596d04f1ea38bea09aaafe7dc8a790440d
Emily Dela Cruz <delacruz.emily@gmail.com>
2019-12-03 22:03:46 -0500

    Add custom field to posts - subtitles

--- data/custom/themes/edc/includes/functions/metabox/cpt-posts.php
+++ data/custom/themes/edc/includes/functions/metabox/cpt-posts.php
@@ -2,13 +2,18 @@
 add_filter( 'rwmb_meta_boxes', 'posts_meta_boxes' );
 function posts_meta_boxes( $meta_boxes) {
     $prefix = '_post_';
-    
+
     $meta_boxes[] = array(
         'title'      => __( 'Post Details', 'textdomain' ),
         'post_types' => array( 'post'),
         'context'    => 'normal',
         'priority'   => '',
         'fields' => array(
+            array(
+                'id'   => $prefix . 'subtitle',
+                'name' => __( 'Subtitle', 'textdomain' ),
+                'type' => 'text',
+            ),
             array(
                 'id'   => $prefix . 'link',
                 'name' => __( 'External Link', 'textdomain' ),
