5f3ad128b2e0d57d2055a3da9c227e671d8eeb4f
Emily Dela Cruz <delacruz.emily@gmail.com>
2020-01-31 21:51:51 -0500

    Fix footer typo. What is 'a help'? 🙃

--- app/components/footer.vue
+++ app/components/footer.vue
@@ -4,7 +4,7 @@
       <div>
         <p class="text-align--r">
           What's your story?
-          <br>What do you need a help with?
+          <br>What do you need help with?
           <br>
           <strong>Let's chat.</strong>
         </p>
