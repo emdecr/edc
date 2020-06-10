923e1b6b1c48f90154fc5c606c49bb2a29e5c6e7
Emily Dela Cruz <delacruz.emily@gmail.com>
2020-06-10 15:57:38 -0400

    Switch to next setup

--- app/plugins/vue-paginate.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import VuePaginate from 'vue-paginate'
-
-Vue.use(VuePaginate);
-// Vue.component('paginate', VuePaginate)
-// Vue.component('paginate-links', VuePaginate)
--- app/store/index.js
+++ app/store/index.js
@@ -1,26 +1,37 @@
-export const state = () => ({
-  mode: null
-});
+import { createStore, applyMiddleware } from "redux";
+import { HYDRATE, createWrapper } from "next-redux-wrapper";
+import thunkMiddleware from "redux-thunk";
+import reducers from "./reducers";
 
-export const mutations = {
-  setMode(state, payload) {
-    state.mode = payload;
+const bindMiddleware = middleware => {
+  if (process.env.NODE_ENV !== "production") {
+    const { composeWithDevTools } = require("redux-devtools-extension");
+    return composeWithDevTools(applyMiddleware(...middleware));
+  }
+  return applyMiddleware(...middleware);
+};
+
+const combinedReducer = reducers;
+
+const reducerWrapper = (state, action) => {
+  if (action.type === HYDRATE) {
+    console.log("HYDRATE - store index.js", state);
+    const nextState = {
+      ...state, // use previous state
+      ...action.payload // apply delta from hydration
+    };
+    // preserve prev state values on client side navigation
+    // if (state.pages) nextState.pages = state.pages;
+    // Attention! This will overwrite client state! Use proper reconciliation.
+    return nextState;
+  } else {
+    console.log("NOT HYDRATE - store index.js", state);
+    return combinedReducer(state, action);
   }
 };
 
-export const actions = {
-  async getMode({ state, commit, dispatch }) {
-    let check = localStorage.getItem("siteTheme");
-    if (check !== null) {
-      commit("setMode", true);
-    } else {
-      return;
-    }
-  }
+const initStore = () => {
+  return createStore(reducerWrapper, bindMiddleware([thunkMiddleware]));
 };
 
-export const getters = {
-  getMode(state) {
-    return state.mode;
-  }
-};
+export const wrapper = createWrapper(initStore);
